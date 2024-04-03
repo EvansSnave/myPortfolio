82e7bfa274d7499fb4eb7af26c17dbbcfc1a8c44
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-04-03 17:22:02 -0500

    create nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'projects', component: MainComponent },
-  { path: 'resume', component: MainComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent },
 ];
