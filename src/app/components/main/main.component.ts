eafb1a56f15fa01df24b03b5df31ff813dcdc563
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-04-03 20:11:26 -0500

    create about component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
