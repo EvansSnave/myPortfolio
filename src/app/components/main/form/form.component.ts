12fe6da943f587246e61641f26290315fe5223b3
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-04-04 14:51:22 -0500

    add all sections

--- src/app/components/main/form/form.component.ts
+++ src/app/components/main/form/form.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
