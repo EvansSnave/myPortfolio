867b96d5749cf3dc286e4fb526cd4f2b700046a4
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-04-05 20:08:18 -0500

    improve type animation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
