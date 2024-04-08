4a4c13d511153e5d5821cb88d20def2ad195cac7
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-04-08 16:44:08 -0500

    add nav overlay

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,10 +10,21 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  constructor(private render: Renderer2, private el: ElementRef) {
+  }
 
-  constructor() { }
+  ngOnInit():void {
+    const nav = this.el.nativeElement.querySelector('nav');
+    this.render.setStyle(nav, 'left', '-30vw');
+  }
 
-  ngOnInit() {
+  toggleNav(burger: HTMLButtonElement, nav: HTMLElement, overlay: HTMLDivElement): void {
+    const isNavOpen = nav.style.left !== '-30vw';
+
+    this.render.setStyle(burger, 'display', isNavOpen ? 'block' : 'none');
+    this.render.setStyle(nav, 'left', isNavOpen ? '-30vw' : '0');
+    this.render.setStyle(overlay, 'display', isNavOpen ? 'none' : 'block');
+    this.render.setStyle(document.body, 'overflow', isNavOpen ? 'scroll' : 'hidden');
   }
 
 }
