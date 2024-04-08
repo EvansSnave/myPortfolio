043cd6e7bd791a62a63074f9eac8ae50833b7d2d
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-04-08 17:34:58 -0500

    finish responsive nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, OnInit, Renderer2, ElementRef} from '@angular/core';
+import { Component, OnInit, Renderer2, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,21 +10,40 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  @ViewChild('nav') nav: ElementRef<HTMLElement>;
+  @ViewChild('overlay') overlay: ElementRef<HTMLDivElement>;
+  @ViewChild('burger') burger: ElementRef<HTMLButtonElement>
+
   constructor(private render: Renderer2, private el: ElementRef) {
+    this.nav = this.el.nativeElement;
+    this.overlay = this.el.nativeElement;
+    this.burger = this.el.nativeElement;
   }
 
   ngOnInit():void {
-    const nav = this.el.nativeElement.querySelector('nav');
-    this.render.setStyle(nav, 'left', '-30vw');
   }
 
   toggleNav(burger: HTMLButtonElement, nav: HTMLElement, overlay: HTMLDivElement): void {
-    const isNavOpen = nav.style.left !== '-30vw';
+    const isNavOpen = nav.style.left === '0px';
 
     this.render.setStyle(burger, 'display', isNavOpen ? 'block' : 'none');
-    this.render.setStyle(nav, 'left', isNavOpen ? '-30vw' : '0');
+    this.render.setStyle(nav, 'left', isNavOpen ? '-30vw' : '0px');
     this.render.setStyle(overlay, 'display', isNavOpen ? 'none' : 'block');
     this.render.setStyle(document.body, 'overflow', isNavOpen ? 'scroll' : 'hidden');
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    let width = (event.target as Window).innerWidth;
+    this.render.setStyle(document.body, 'overflow', 'scroll');
+    if (width > 1024) {
+      this.render.setStyle(this.nav.nativeElement, 'left', '0px');
+      this.render.setStyle(this.burger.nativeElement, 'display', 'none');
+    } else {
+      this.render.setStyle(this.nav.nativeElement, 'left', '-30vw');
+      this.render.setStyle(this.overlay.nativeElement, 'display', 'none');
+      this.render.setStyle(this.burger.nativeElement, 'display', 'block');
+    }
   }
 
 }
